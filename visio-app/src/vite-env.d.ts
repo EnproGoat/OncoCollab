1643b89491ad83f24b824f8b283586880b9650c9
EnproGoat <lokcedriclok@gmail.com>
2026-02-06 15:40:34 +0100

    chore : build/docker update

--- visio-app/src/vite-env.d.ts
+++ visio-app/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_WS_URL: string
+  readonly VITE_ROOM_ID: string
+  readonly VITE_STUN_URL: string
+  readonly VITE_TURN_URL: string
+  readonly VITE_TURN_USERNAME: string
+  readonly VITE_TURN_PASSWORD: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
